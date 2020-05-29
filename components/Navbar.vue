52abdc56db1b95285fe7238ddd4a09bf9ae161d7
Cubic <44077108+Niseyami@users.noreply.github.com>
2020-05-29 15:17:20 +1000

    tons of stuff

---
+++ components/Navbar.vue
@@ -0,0 +1,76 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nuxt-link class="navbar-brand" to="/home">
+      <svg
+        width="48"
+        height="40"
+        viewBox="0 0 60 50"
+        xmlns="http://www.w3.org/2000/svg"
+        style="padding-top: 5px; padding-bottom: 0px"
+      >
+        <g fill-rule="evenodd" transform="scale(0.125)">
+          <path
+            d="M139 330l-1-2c-2-4-2-8-1-13H29L189 31l67 121 22-16-67-121c-1-2-9-14-22-14-6 0-15 2-22 15L5 303c-1 3-8 16-2 27 4 6 10 12 24 12h136c-14 0-21-6-24-12z"
+            stroke="#00C58E"
+            stroke-width="4"
+            fill="rgba(0,197,142,1)"
+          />
+          <path
+            d="M447 304L317 70c-2-2-9-15-22-15-6 0-15 3-22 15l-17 28v54l39-67 129 230h-49a23 23 0 0 1-2 14l-1 1c-6 11-21 12-23 12h76c3 0 17-1 24-12 3-5 5-14-2-26z"
+            stroke="#108775"
+            stroke-width="4"
+            fill="rgba(16,135,117,1)"
+          />
+          <path
+            d="M376 330v-1l1-2c1-4 2-8 1-12l-4-12-102-178-15-27h-1l-15 27-102 178-4 12a24 24 0 0 0 2 15c4 6 10 12 24 12h190c3 0 18-1 25-12zM256 152l93 163H163l93-163z"
+            stroke="#2F495E"
+            stroke-width="4"
+            fill="rgba(47,73,94,1)"
+          />
+        </g>
+      </svg>
+      <div style="float: right; margin-top: 6px; margin-left: 4px;">
+        Agriculture
+      </div>
+    </nuxt-link>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNavAltMarkup"
+      aria-controls="navbarNavAltMarkup"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div id="navbarNavAltMarkup" class="collapse navbar-collapse">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link" href="#">The Issues</a>
+        <nuxt-link to="/contact" class="nav-item nav-link"
+          >Contact Us</nuxt-link
+        >
+        <a class="nav-item nav-link" href="#">Donate</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style>
+nav {
+  position: absolute;
+  z-index: 99;
+}
+
+.navbar {
+  padding-left: 10%;
+}
+
+.navbar-nav {
+  margin-top: 4px;
+}
+
+#navbarNavAltMarkup {
+  padding-right: 10%;
+}
+</style>
---
+++ pages/contact.vue
@@ -0,0 +1,235 @@
+<template>
+  <div style="position: relative; min-height: 100vh;">
+    <navbar />
+    <div class="container main-content">
+      <div class="row justify-content-between" style="margin-top: 4rem">
+        <div class="col-sm-7" style="margin-bottom: 6rem;">
+          <div class="card shadow-sm">
+            <div class="card-body">
+              <h5 class="card-title title">Contact us</h5>
+              <p class="card-text">
+                We'd love to hear from you!
+              </p>
+              <hr />
+              <p class="card-text">
+                Feedback:
+                <a href="mailto:feedback@example.org">feedback@example.org</a
+                ><br />
+                Phone: <a href="tel:+12065550100">+1 206 555 0100</a><br />
+                Fax: <a href="tel:+61212345678">+61 2 1234 5678</a><br />
+                Legal Inquiries:
+                <a href="mailto:legal@example.org">legal@example.org</a>
+              </p>
+              <hr />
+              <h5 class="card-title title" style="font-size:2rem">
+                Got a question?
+              </h5>
+              <form>
+                <div class="form-group">
+                  <label for="inputEmail4">Email</label>
+                  <input
+                    id="inputEmail"
+                    type="email"
+                    class="form-control"
+                    placeholder="Email"
+                    aria-describedby="emailHelp"
+                  />
+                  <small id="emailHelp" class="form-text text-muted"
+                    >We'll never share your email with anyone else.</small
+                  >
+                </div>
+                <div class="form-group">
+                  <label for="TextArea">What do you want to know?</label>
+                  <textarea
+                    id="TextArea"
+                    class="form-control"
+                    rows="3"
+                  ></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Send</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-5">
+          <div class="card shadow-sm" style="margin-bottom: 6rem;">
+            <div class="card-body">
+              <h5 class="card-title title" style="font-size: 2rem;">
+                Find us on social media
+              </h5>
+              <hr />
+              <p class="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <div>
+                <a href=""><i class="fab fa-steam-square fa-5x"></i></a>
+                <a href=""><i class="fab fa-facebook-square fa-5x"></i></a>
+                <a href=""><i class="fab fa-github-square fa-5x"></i></a>
+                <a href=""><i class="fab fa-snapchat-square fa-5x"></i></a>
+                <a href=""><i class="fab fa-twitter-square fa-5x"></i></a>
+                <a href=""><i class="fab fa-tumblr-square fa-5x"></i></a>
+                <a href=""><i class="fab fa-instagram-square fa-5x"></i></a>
+                <a href=""><i class="fab fa-linkedin fa-5x"></i></a>
+                <a href=""><i class="fab fa-youtube-square fa-5x"></i></a>
+                <a href=""><i class="fab fa-reddit-square fa-5x"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="footer">
+      <div class="container-fluid">
+        <span class="text-white-50" style="margin-left: 2rem;"
+          >&copy; Niseyami 2020 / Github:
+          <a href="https://github.com/Niseyami" class="text-white-50"
+            >https://github.com/Niseyami</a
+          ></span
+        >
+        >
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  align-items: normal;
+}
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 50px;
+  color: #35495e;
+  letter-spacing: 1px;
+  margin-bottom: 0.5rem;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+hr {
+  margin-bottom: 25px;
+}
+
+a {
+  color: #35495e;
+}
+
+a:hover {
+  color: #2c3d4e;
+}
+
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 2.5rem;
+  line-height: 2.5rem;
+  background-color: #2e3338;
+}
+
+$t-duration: 600ms;
+$t-delay: 150ms;
+
+.page-enter-active,
+.page-leave-active {
+  transition-duration: $t-duration * 2;
+
+  &::before,
+  &::after {
+    content: '';
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    display: block;
+    width: 100%;
+    height: 50%;
+    transition-property: opacity, transform;
+    transition-timing-function: ease-in-out;
+  }
+
+  &::before {
+    background-color: #2e2e2e;
+  }
+
+  &::after {
+    top: 50%;
+    background-color: #2e2e2e;
+  }
+}
+
+.page-leave {
+  &::before,
+  &::after {
+    transform: scaleX(0);
+  }
+}
+
+.page-leave-active {
+  &::before {
+    transition-duration: $t-duration;
+  }
+
+  &::after {
+    transition-duration: $t-duration - $t-delay;
+    transition-delay: $t-delay;
+  }
+}
+
+.page-leave-to {
+  &::before,
+  &::after {
+    transform: scale(1);
+    transform-origin: left;
+  }
+}
+
+.page-enter {
+  &::before,
+  &::after {
+    transform: scaleX(1);
+  }
+}
+
+.page-enter-active {
+  &::before {
+    transition-duration: $t-duration;
+  }
+
+  &::after {
+    transition-duration: $t-duration - $t-delay;
+    transition-delay: $t-delay;
+  }
+}
+
+.page-enter-to {
+  &::before,
+  &::after {
+    transform: scaleX(0);
+    transform-origin: right;
+  }
+}
+</style>
+
+<script>
+import Navbar from '~/components/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  },
+  head: {
+    title: 'Agriculture'
+  }
+}
+</script>
